15002d7fbc0b13cb8d1adbf3605c1d73f298964f
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-02-09 17:11:40 -0300

    feat(UsersApp): Create model for User

--- server/UserApp/models.py
+++ server/UserApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    UserId = models.AutoField(primary_key=True)
+    UserName = models.CharField(max_length=500)
+    UserPassword = models.CharField(max_length=500)
+    UserEmail = models.EmailField(max_length=500)
