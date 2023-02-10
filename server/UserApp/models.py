fcca753a9dcb4afaa52f041a62136fedf9e84d3d
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-02-10 10:08:19 -0300

    feat(Database): Create migration for user

--- server/UserApp/models.py
+++ server/UserApp/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    UserId = models.AutoField(primary_key=True)
-    UserName = models.CharField(max_length=500)
-    UserPassword = models.CharField(max_length=500)
-    UserEmail = models.EmailField(max_length=500)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=500)
+    password = models.CharField(max_length=500)
+    email = models.EmailField(max_length=500)
+
+    class Meta:
+        db_table = 'users'
